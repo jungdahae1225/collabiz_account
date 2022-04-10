4123a21fb630e8bd8f3f73c1184405e9c534c7d7
jungdahae1225 <jungdahae1225@gmail.com>
2022-04-11 01:42:20 +0900

    add EEMail Entity

---
+++ src/main/java/com/example/collabiz_account/EEmail.java
@@ -0,0 +1,55 @@
+package com.example.collabiz_account;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class EEmail {
+    @Id
+    @GeneratedValue
+    @Column(name = "email_id")
+    private Long id;
+
+    @Email
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @OneToOne(mappedBy = "email", fetch = FetchType.LAZY)
+    private Account account;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    private boolean emailVerified;
+
+    @JsonIgnore
+    private String emailCheckToken;
+
+    //private LocalDateTime emailCheckTokenGeneratedAt;
+
+    public void generateEmailCheckToken() {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        uuid = uuid.substring(0, 10);
+        this.emailCheckToken = uuid;
+        //this.emailCheckTokenGeneratedAt = LocalDateTime.now();
+    }
+
+    public boolean isValidToken(String token) {
+        return this.emailCheckToken.equals(token);
+    }
+
+    public void completeSignUp() {
+        this.setEmailVerified(true);
+    }
+
+}
