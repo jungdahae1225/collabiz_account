c6b5fc35fddd4f4326ea9c6cc6714ed752b24e65
jungdahae1225 <jungdahae1225@gmail.com>
2022-04-18 16:55:05 +0900

    err log

--- src/main/java/com/example/collabiz_account/CollabizAccountApplication.java
+++ src/main/java/com/example/collabiz_account/CollabizAccountApplication.java
@@ -3,6 +3,9 @@ package com.example.collabiz_account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Parameter value [jdahae1225@naver.com] did not match expected type [com.example.collabiz_account.EEmail (n/a)]
+//오류해결필요
+
 @SpringBootApplication
 public class CollabizAccountApplication {
 
